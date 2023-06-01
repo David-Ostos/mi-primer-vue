6451e4220b7b0f1f5b22da78fe7772b58187fae4
David <davidostos2@gmail.com>
2023-06-01 14:04:55 -0400

    iniciando proyecto mi-primer-vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const name = 'Vue dinamico';
+const styleColor = "color:blue";
+</script>
+
+<template>
+  <h1>Hola {{ name }}</h1>
+  <h2 v-bind:style="styleColor" > Soy azul</h2>
+</template>
+
+<style>
+
+h1 {
+  color: #8c8c8c;
+}
+</style>
